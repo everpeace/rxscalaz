95ef47159a713dbbafcd5d99477ce8a5dc05a1da
Shingo Omura <everpeace@gmail.com>
2014-12-16 23:19:57 -0800

    Change equality used in test to list based equality. fix #1

--- src/test/scala/rx/lang/scala/scalaz/ImplicitsForTest.scala
+++ src/test/scala/rx/lang/scala/scalaz/ImplicitsForTest.scala
@@ -14,16 +14,25 @@ import scalaz.scalacheck.ScalaCheckBinding._
  */
 object ImplicitsForTest {
 
-  // Equality based on sequenceEqual() method.
+  // Equality based on toBlocking.toList method.
   implicit def observableEqual[A](implicit eqA: Equal[A]) = new Equal[Observable[A]] {
     def equal(a1: Observable[A], a2: Observable[A]) = {
-      val p = Promise[Boolean]
-      val sub = a1.sequenceEqual(a2).firstOrElse(false).subscribe(v => p.success(v))
-      try {
-        Await.result(p.future, Duration.Inf)
-      } finally {
-        sub.unsubscribe()
-      }
+      import scalaz._, Scalaz._
+      a1.toBlocking.toList === a2.toBlocking.toList
+      //      val p = Promise[Boolean]
+      //      val sub = a1.sequenceEqualWith(a2)(_ === _).subscribe(v => p.success(v))
+      //      try {
+      //        val ret = Await.result(p.future, Duration.Inf)
+      //        if (!ret) {
+      //          val x = a1.toBlocking.toList
+      //          val y = a2.toBlocking.toList
+      //          val z = a1.sequenceEqualWith(a2)(eqA.equal).toBlocking.toList
+      //          val aa = x === y
+      //        }
+      //        ret
+      //      } finally {
+      //        sub.unsubscribe()
+      //      }
     }
   }
 
